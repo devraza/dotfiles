3cb63cdf6f11e2b0b864ff90d5c0b786df1da51b
Muhammad Nauman Raza <thedevraza@proton.me>
2022-12-07 19:55:59 +0000

    feat: update dotfiles

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,5 +2,45 @@ if status is-interactive
    starship init fish | source
 end
 
-# Change location of starship's config file
-set STARSHIP_CONFIG ~/.config/fish/starship.toml
+# Set Editor
+set EDITOR "emacsclient -c"
+
+# HiDPI
+set GDK_SCALE 2
+set GDK_DPI_SCALE 0.5
+
+# Create XDG_RUNTIME_DIR
+set XDG_RUNTIME_DIR "/tmp/$USER"
+mkdir -p $XDG_RUNTIME_DIR
+chmod 0700 $XDG_RUNTIME_DIR
+
+# Start X on login
+if status is-login
+   startx
+end
+
+# Disable the greeting
+set fish_greeting
+
+# Zoxide
+zoxide init fish | source
+
+# Aliases
+# General Aliases
+alias ls 'exa -1l --sort=size'
+alias l 'exa -1al --sort=size'
+alias mkdir 'mkdir -p'
+
+alias .. 'cd ..'
+alias ... 'cd ../..'
+alias .3 'cd ../../..'
+alias .4 'cd ../../../..'
+
+# XBPS Aliases
+alias xbps-i 'sudo xbps-install'
+alias xbps-isyu 'sudo xbps-install -Syu'
+alias xbps-is 'sudo xbps-install -S'
+alias xbps-r 'sudo xbps-remove'
+alias xbps-roo 'sudo xbps-remove -oO'
+alias xbps-q 'xbps-query'
+alias xbps-qrs 'xbps-query -Rs'
